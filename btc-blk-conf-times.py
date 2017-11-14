44917f7b9d534e3e19c24bcabf0553dd2a674f16
William Johnson <willrayjohnson@gmail.com>
2017-11-13 21:51:25 -0600

    Implemented fetching of bitcoin block confirm times

--- btc-blk-conf-times.py
+++ btc-blk-conf-times.py
@@ -2,5 +2,40 @@
 
 import json
 import subprocess
+import sys
+import datetime
 
+blockchainCLI = 'bitcoin-cli'
+try:
+    blockCount = int(sys.argv[1])
+except IndexError:
+    blockCount = 1
 
+countProc = subprocess.Popen( [blockchainCLI, 'getblockcount'],
+                            stdout=subprocess.PIPE )
+blockHeight = int(countProc.stdout.read().decode('utf-8'))
+
+def getBlockConfirmTime(blockNum):
+    hashProc = subprocess.Popen( [blockchainCLI, 'getblockhash', str(blockNum)],
+                                stdout=subprocess.PIPE )
+    blockHash = hashProc.stdout.read().decode('utf-8')
+    
+    blockProc = subprocess.Popen( [blockchainCLI, 'getblock', blockHash],
+                                stdout=subprocess.PIPE )
+    blockData = blockProc.stdout.read().decode('utf-8')
+    block = json.loads(blockData)
+    blockConfirmTimestamp = int(block['time'])
+    return blockConfirmTimestamp
+
+for block in range(blockCount):
+    currentBlockTime = getBlockConfirmTime(blockHeight - block)
+    previousBlockTime = getBlockConfirmTime(blockHeight - block - 1)
+    timeToConfirmSec = currentBlockTime - previousBlockTime
+    timeToConfirmBlock = str(datetime.timedelta(
+                            seconds=(timeToConfirmSec)))
+    blockConfirmTime = datetime.datetime.fromtimestamp(
+            currentBlockTime).strftime('%m-%d-%Y %H:%M:%S')
+
+    print("Block %d" % (blockHeight - block,))
+    print("Block Confirmed at: %s" % (blockConfirmTime,))
+    print("Time to Confirm Block: %s" % (timeToConfirmBlock,))
